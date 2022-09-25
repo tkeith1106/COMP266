b52d8848278968efcf50f43aa0b4a61bde78de4a
Tyhler Keith <tkeith1106@gmail.com>
2022-09-25 16:27:15 -0600

    unit 4; edit, then add javascript into my site from another source.

--- root/javascript/script.js
+++ root/javascript/script.js
@@ -1,12 +1,10 @@
 // SOURCE: https://www.w3schools.com/howto/howto_js_accordion.asp
 let acc = document.getElementsByClassName("drop_d_box_item");
 let i;
-
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         // uses a click type to turn the content on or off
         this.classList.toggle("active");
-
         // turns the content either to block or to none
         let drop_d_box_content = this.nextElementSibling;
         if (drop_d_box_content.style.display === "block") {
